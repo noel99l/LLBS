aa7ab7c305254259287f0b95414fa79a5e1c9773
noel99l <noel1991@hotmail.co.jp>
2020-03-03 07:20:42 +0000

    [update]トップページレイアウト作成 skipper削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -117,33 +117,6 @@ function previewFile() {
 }
 
 
-$(document).on('turbolinks:load', function(){
-  // オプションを指定してSkipprの実行
-  $("#theTarget").skippr({
-    // スライドショーの変化（"fade" or "slide"）
-    transition : 'fade',
-    // 変化にかかる時間（ミリ秒）
-    speed : 1000,
-    // easingの種類
-    easing : 'easeOutQuart',
-    // ナビゲーションの形（"block" or "bubble"）
-    navType : 'bubble',
-    // 子要素の種類（"div" or "img"）
-    childrenElementType : 'div',
-    // ナビゲーション矢印の表示（trueで表示）
-    arrows : false,
-    // スライドショーの自動再生（falseで自動再生なし）
-    autoPlay : true,
-    // 自動再生時のスライド切替間隔（ミリ秒）
-    autoPlayDuration : 2500,
-    // キーボードの矢印キーによるスライド送りの設定（trueで有効）
-    keyboardOnAlways : true,
-    // 1枚目のスライド表示時に戻る矢印を表示するかどうか（falseで非表示）
-    hidePrevious : false
-  });
-});
-
-
 var data = {'data-format': 'yyyy-MM-dd hh:mm:ss' };
 $(function(){
     $('.datepicker').attr(data);
