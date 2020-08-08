b13594fd9aa59da0fb637aaae5beb6f011eeadb6
Yusuke Kataoka <y.kataoka@adseed.co.jp>
2020-08-09 03:49:08 +0900

    [Add] モバイルメニュー追加、レスポンシブ対応準備

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,98 +22,105 @@
 //= require_tree .
 
 
-// タイムテーブル並べ替え
-$(function(){
-  // トークンの取得
-   $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-      var token;
-      if (!options.crossDomain) {
-        token = $('meta[name="csrf-token"]').attr('content');
-        if (token) {
-            return jqXHR.setRequestHeader('X-CSRF-Token', token);
-         }
-      }
-  });
+// モバイルメニュー開閉
+$('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#mobile-menu').fadeToggle();
+    event.preventDefault();
+});
 
-  // テーブルの横幅をとってくる
-   function fixPlaceHolderWidth(event, ui){
+// タイムテーブル並べ替え
+$(function() {
+    // トークンの取得
+    $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+        var token;
+        if (!options.crossDomain) {
+            token = $('meta[name="csrf-token"]').attr('content');
+            if (token) {
+                return jqXHR.setRequestHeader('X-CSRF-Token', token);
+            }
+        }
+    });
+
+    // テーブルの横幅をとってくる
+    function fixPlaceHolderWidth(event, ui) {
         // adjust placeholder td width to original td width
-        ui.children().each(function(){
+        ui.children().each(function() {
             $(this).width($(this).width());
         });
         return ui;
     };
-  // sortable
+    // sortable
     var el = document.getElementById('sortable');
     if (el !== null) {
-      return sortable = Sortable.create(el, {
-        // テーブルの横幅をそのままに
-        start: function(event, ui){
-            ui.placeholder.height(ui.helper.outerHeight());
-        },
-        helper: fixPlaceHolderWidth,
-        // 移動した時
-        onUpdate: function(evt) {
-          return $.ajax({
-            url: 'timetable/sort',
-            type: 'patch',
-            data:{
-              from: evt.oldIndex,
-              to: evt.newIndex
+        return sortable = Sortable.create(el, {
+            // テーブルの横幅をそのままに
+            start: function(event, ui) {
+                ui.placeholder.height(ui.helper.outerHeight());
+            },
+            helper: fixPlaceHolderWidth,
+            // 移動した時
+            onUpdate: function(evt) {
+                return $.ajax({
+                    url: 'timetable/sort',
+                    type: 'patch',
+                    data: {
+                        from: evt.oldIndex,
+                        to: evt.newIndex
+                    }
+                });
             }
-          });
-        }
-      });
-  };
+        });
+    };
 });
 
 
 function initMap(latlng) {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: latlng,
-    zoom: 17
-  });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: latlng,
+        zoom: 17
+    });
 
-  var marker = new google.maps.Marker({
-    position: latlng,
-    map: map
-  });
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map
+    });
 }
 
 function getLatLng(address) {
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({
-    address: address
-  }, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      for (var i in results) {
-        if (results[i].geometry) {
-          var latlng = results[i].geometry.location;
-          initMap(latlng)
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        address: address
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            for (var i in results) {
+                if (results[i].geometry) {
+                    var latlng = results[i].geometry.location;
+                    initMap(latlng)
+                }
+            }
         }
-      }
-    }
-  });
+    });
 }
 
 
 // 商品新規登録時に、画像を選択した段階でプレビューが表示されるようにする
 function previewFile() {
-  var preview = document.getElementById('image_preview');//どこでプレビューするか指定。'img[name="preview"]'などにすればimg複数あっても指定できます。
-  var file    = document.querySelector('input[type=file]').files[0];//'input[type=file]'で投稿されたファイル要素の0番目を参照します。input[type=file]にmutipleがなくてもこのコードになります。
-  var reader  = new FileReader();
+    var preview = document.getElementById('image_preview'); //どこでプレビューするか指定。'img[name="preview"]'などにすればimg複数あっても指定できます。
+    var file = document.querySelector('input[type=file]').files[0]; //'input[type=file]'で投稿されたファイル要素の0番目を参照します。input[type=file]にmutipleがなくてもこのコードになります。
+    var reader = new FileReader();
 
-  reader.addEventListener("load", function () {
-    preview.src = reader.result;//めちゃめちゃ長い文字列が引き渡されます。ユーザーのファイルパスに紐付かない画像情報だと思います。
-  }, false);
+    reader.addEventListener("load", function() {
+        preview.src = reader.result; //めちゃめちゃ長い文字列が引き渡されます。ユーザーのファイルパスに紐付かない画像情報だと思います。
+    }, false);
 
-  if (file) {
-    reader.readAsDataURL(file);//ここでreaderのメソッドに引数としてfileを入れます。ここで、readerのaddEventListenerが発火します。
-  }
+    if (file) {
+        reader.readAsDataURL(file); //ここでreaderのメソッドに引数としてfileを入れます。ここで、readerのaddEventListenerが発火します。
+    }
 }
 
-var data = {'data-format': 'yyyy-MM-dd hh:mm:ss' };
-$(function(){
+var data = { 'data-format': 'yyyy-MM-dd hh:mm:ss' };
+$(function() {
     $('.datepicker').attr(data);
     $('.datepicker').datetimepicker();
 });
